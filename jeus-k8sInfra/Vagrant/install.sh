ae2d87800df6c5b3076a293644e47fc4a07ee7da
sambunarayan <soyou30257@gmail.com>
2021-09-11 17:34:23 +0900

    20210911 jeus-k8s
    ・Added install comment

--- jeus-k8sInfra/Vagrant/install.sh
+++ jeus-k8sInfra/Vagrant/install.sh
@@ -2,10 +2,10 @@
 # install helm
 export DESIRED_VERSION=v3.2.1; /root/Jeus-cloudes/jeus-k8s-cloud/helm/helm-install.sh
 
-#
+# add helm chart repository
 helm repo add edu https://iac-source.github.io/helm-charts
-#
+# update helm
 helm repo update
 
 # install docker registry
-sh /root/Jeus-cloudes/docker/create-registry.sh
+sh /root/Jeus-cloudes/jeus-k8s-cloud/docker/create-registry.sh
