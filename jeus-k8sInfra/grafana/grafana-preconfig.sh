db793f1d9baa17642ca4c22e4b3094155ffc233f
sambunarayan <soyou30257@gmail.com>
2021-10-20 22:21:14 +0900

    20211020 Build jeus-k8s
    ・Fixed grafana installation files.

--- jeus-k8sInfra/grafana/grafana-preconfig.sh
+++ jeus-k8sInfra/grafana/grafana-preconfig.sh
@@ -21,7 +21,7 @@ echo "[Step 2/4] ok"
 nfsdir=/nfs_shared/grafana
 echo "[Step 3/4] Task [Create NFS directory for grafana]"
 if [ ! -e "$nfsdir"  ]; then
-  ~/_Book_k8sInfra/ch6/6.4.1/nfs-exporter.sh grafana
+  ~/jeus-k8sInfra/grafana/nfs-exporter.sh grafana
   chown 1000:1000 $nfsdir
   echo "$nfsdir created"
   echo "[Step 3/4] Successfully completed"
@@ -34,7 +34,7 @@ fi
 echo "[Step 4/4] Task [Create PV,PVC for grafana]"
 pvc=$(kubectl get pvc grafana -o jsonpath={.metadata.name} 2> /dev/null)
 if [ "$pvc" == "" ]; then
-  kubectl apply -f ~/_Book_k8sInfra/ch6/6.4.1/grafana-volume.yaml
+  kubectl apply -f ~/jeus-k8sInfra/grafana/grafana-volume.yaml
   echo "[Step 4/4] Successfully completed"
 else
   echo "[Step 4/4] failed: grafana pv,pvc already exist"
--- jeus-k8sInfra/grafana/nfs-exporter.sh
+++ jeus-k8sInfra/grafana/nfs-exporter.sh
@@ -6,7 +6,7 @@ fi
 
 if [[ ! -d $nfsdir ]]; then
   mkdir -p $nfsdir
-  echo "$nfsdir 192.168.1.0/24(rw,sync,no_root_squash)" >> /etc/exports
+  echo "$nfsdir 192.168.2.0/24(rw,sync,no_root_squash)" >> /etc/exports
   if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]; then
     systemctl enable nfs
   fi
